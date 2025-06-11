b1bba5b6ed47fb080f345b69359dbb3a1fa1d3ee
Sabrina Wang <sw1622@ic.ac.uk>
2025-06-11 12:45:55 +0100

    added comments

--- demo/src/main.c
+++ demo/src/main.c
@@ -2,17 +2,29 @@
  * Copyright (c) 2023 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ * adv_only_test: 50 packets per cycle, 10 cycles in total
+ * packet number is updated in manufacturer data
  */
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-/* STEP 3 - Include the header file of the Bluetooth LE stack */
+
+/*  Include the header file of the Bluetooth LE stack 
+ *       Provides core Bluetooth initialization and control APIs,
+ *       including enabling the Bluetooth stack and managing device states.
+*/
 #include <zephyr/bluetooth/bluetooth.h>
+
+/*  Include the header file of the BLE GAP（Generic Access Profile）
+ *       GAP (Generic Access Profile) defines core Bluetooth behaviors,
+ *       including advertising, scanning, connection establishment,
+ *       device name/role configuration, and connection parameter control. 
+ */
 #include <zephyr/bluetooth/gap.h>
 
 //#include <dk_buttons_and_leds.h>
 
-LOG_MODULE_REGISTER(Lesson2_Exercise1, LOG_LEVEL_INF);
+LOG_MODULE_REGISTER(ADV_ONLY_TEST, LOG_LEVEL_INF);
 
 #define DEVICE_NAME CONFIG_BT_DEVICE_NAME
 #define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
@@ -20,20 +32,20 @@ LOG_MODULE_REGISTER(Lesson2_Exercise1, LOG_LEVEL_INF);
 #define RUN_STATUS_LED DK_LED1
 #define RUN_LED_BLINK_INTERVAL 1000
 
-#define REPEAT_COUNT 50  //每次广播开启期间发送packet的次数
+#define REPEAT_COUNT 50  // broadcast 50 adv packets in each cycle
 
-/* 定义工作队列和延迟工作结构 */
+/*  Define delayed work item*/
 static struct k_work_delayable ble_broadcast_work;
-static int broadcast_count = 0;  // 广播计数器
+static int broadcast_count = 0;  // adv cycle count
 
-/* STEP 1 - Create an LE Advertising Parameters variable */
+/* BLE Advertising Parameters variable */
 static struct bt_le_adv_param *adv_param =
 	BT_LE_ADV_PARAM(BT_LE_ADV_OPT_NONE, /* No options specified */
 			800, /* Min Advertising Interval 500ms (800*0.625ms) */
 			800, /* Max Advertising Interval 500ms (800*0.625ms) */
 			NULL); /* Set to NULL for undirected advertising */
 
-/* STEP 2.1 - Declare the Company identifier (Company ID) */
+/* Declare the Company identifier (Company ID) */
 #define COMPANY_ID_CODE 0x0059
 
 typedef struct adv_mfg_data {
@@ -44,13 +56,13 @@ typedef struct adv_mfg_data {
 /* Define and initialize a variable of type adv_mfg_data_type */
 static adv_mfg_data_type adv_mfg_data = { COMPANY_ID_CODE, 0x0000 };
 
-/* STEP 4.1.1 - Declare the advertising packet */
+/* Declare the advertising packet */
 static const struct bt_data ad[] = {
-	/* STEP 4.1.2 - Set the advertising flags */
-	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
-	/* STEP 4.1.3 - Set the advertising packet data  */
-	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
-	BT_DATA(BT_DATA_MANUFACTURER_DATA, (unsigned char *)&adv_mfg_data, sizeof(adv_mfg_data)),
+	/* Set the advertising flags */
+	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR), // no BR/EDR support
+	/* Set the advertising packet data: device name and manufacturer data */
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN), 
+	BT_DATA(BT_DATA_MANUFACTURER_DATA, (unsigned char *)&adv_mfg_data, sizeof(adv_mfg_data)),   
 
 };
 
@@ -65,10 +77,11 @@ static void update_advertising_data(void) {
     }
 }
 
-/* BLE广播开启函数 */
+/* BLE start broadcast */
 void start_ble_broadcast(void)
 {
     int err;
+    // adv date: ad, no scan response data
     err = bt_le_adv_start(adv_param, ad, ARRAY_SIZE(ad), NULL, 0);
     if (err) {
         LOG_ERR("Advertising failed to start (err %d)", err);
@@ -78,7 +91,7 @@ void start_ble_broadcast(void)
 }
 
 
-/* BLE广播停止函数 */
+/* BLE stop broadcast */
 void stop_ble_broadcast(void)
 {
     int err;
@@ -90,10 +103,10 @@ void stop_ble_broadcast(void)
     }
 }
 
-/* 延迟工作处理函数 */
+/* delayed work handler */
 void ble_broadcast_handler(struct k_work *work)
 {
-    if (broadcast_count < 10) {  // 只重复10次
+    if (broadcast_count < 10) {  // 10 broadcast cycles
        start_ble_broadcast();
 
         for (int i = 0; i < REPEAT_COUNT; i++) {
@@ -104,7 +117,7 @@ void ble_broadcast_handler(struct k_work *work)
 
         stop_ble_broadcast(); // Stop advertising after broadcasting is done
 	    LOG_INF("Current sequence number: %u", adv_mfg_data.number);
-        k_work_schedule(&ble_broadcast_work, K_SECONDS(30));  // 30秒后再次开启广播
+        k_work_schedule(&ble_broadcast_work, K_SECONDS(30));  // Delay for the broadcast cycle interval 30s
         broadcast_count++;
     } 
     else {
@@ -117,7 +130,7 @@ int main(void)
 	//int blink_status = 0;
 	int err;
 
-	LOG_INF("Starting Lesson 2 - Exercise 1 \n");
+	LOG_INF("bi-direct BLEnd: adv only test \n");
     /*
     err = dk_leds_init();
 	if (err) {
@@ -140,11 +153,11 @@ int main(void)
 
 
 
-	/* 初始化延迟工作*/
+	/* init delay*/
     k_work_init_delayable(&ble_broadcast_work, ble_broadcast_handler); 
 
-    /* 开始第一次广播（0延迟） */
-    k_work_schedule(&ble_broadcast_work, K_NO_WAIT);  // 立即开始第一次广播
+    /* start the first cycle, no delay） */
+    k_work_schedule(&ble_broadcast_work, K_NO_WAIT);  
 
 	/*for (;;) {
 		dk_set_led(RUN_STATUS_LED, (++blink_status) % 2);
